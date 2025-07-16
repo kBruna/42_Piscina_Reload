83f2441d056db02bc6007421b79f0bb8ede570fa
buehara <buehara@student.42sp.org.br>
2025-07-16 18:57:38 -0300

    edit: ex13 and ex19

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: buehara <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 14:32:22 by buehara           #+#    #+#             */
-/*   Updated: 2025/07/16 17:19:52 by buehara          ###   ########.fr       */
+/*   Updated: 2025/07/16 18:56:16 by buehara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@ void	ft_putchar(char c);
 void	ft_putstr(char *str);
 int		ft_strcmp(char *s1, char *s2);
 void	ft_swap(int *a, int *b);
+
+#include <unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putstr(char *str)
 {
@@ -29,12 +35,14 @@ void	ft_putstr(char *str)
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	while (*s1 || *s2)
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if ((*s1 < *s2) || (*s1 > *s2))
-			return (*s1 - *s2);
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }
@@ -52,25 +60,20 @@ void	ft_sort_params(int argc, char **argv, int *args)
 {
 	int	ctrl_out;
 	int	ctrl_in;
-	int	swap;
 
 	ctrl_out = 0;
-	while (ctrl_out < argc - 2)
+	while (ctrl_out < argc)
 	{
-		swap = 0;
-		ctrl_in = 1;
-		while (ctrl_in < (argc - ctrl_out - 1))
+		ctrl_in = ctrl_out + 1;
+		while (argc - 1 > ctrl_in)
 		{
-			if (ft_strcmp(argv[ctrl_in], argv[ctrl_in + 1]) > 0)
+			if (ft_strcmp(argv[ctrl_out], argv[ctrl_in]) > 0)
 			{
-				ft_swap(&args[ctrl_in - 1], &args[ctrl_in]);
-				swap = 1;
+				ft_swap(&args[ctrl_out], &args[ctrl_in]);
 			}
 			ctrl_in++;
 		}
 		ctrl_out++;
-		if (swap == 0)
-			break ;
 	}
 }
 
@@ -84,12 +87,12 @@ int	main(int argc, char **argv)
 		ctrl = 1;
 		while (argc > ctrl)
 		{
-			args[ctrl - 1] = ctrl;
+			args[ctrl] = ctrl;
 			ctrl++;
 		}
 		ft_sort_params(argc, argv, args);
-		ctrl = 0;
-		while (ctrl < argc - 1)
+		ctrl = 1;
+		while (ctrl < argc)
 		{
 			ft_putstr(argv[args[ctrl]]);
 			ft_putchar('\n');
